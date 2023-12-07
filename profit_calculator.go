7b796f4e2baad02dc0e74592e1b5ec15d2d744b9
Alex Böhm <tools@boehm.work>
2023-12-07 18:54:15 +0100

    feat: add functions to exercise `profit_calculator`

--- profit_calculator.go
+++ profit_calculator.go
@@ -4,26 +4,35 @@ import (
 	"fmt"
 )
 
+// getUserInput prompts the user with a question and returns the user's input as a float64.
+func getUserInput(question string) float64 {
+	var userInput float64
+	fmt.Print(question)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+// calculateFinancial calculates the "Earnings before tax" (EBT), "Earnings after tax" (profit), and the ratio.
+// It takes the revenue, expenses, and tax rate as input parameters and returns the calculated EBT, profit, and ratio.
+func calculateFinancial(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
 func main() {
 	var revenue float64
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	revenue = getUserInput("Revenue: ")
+	expenses = getUserInput("Expenses: ")
+	taxRate = getUserInput("Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	ebt, profit, ratio := calculateFinancial(revenue, expenses, taxRate)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	// Calculate the "Earnings before tax" (EBT), "Earnings after tax" (profit) and the ratio
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
-
-	fmt.Printf("Earnings before tax: %.2f\n", ebt)
-	fmt.Printf("Earnings after tax: %.2f\n", profit)
-	fmt.Printf("Ratio: %.2f\n", ratio)
+	fmt.Printf("Earnings before tax: %.0f\n", ebt)
+	fmt.Printf("Earnings after tax: %.0f\n", profit)
+	fmt.Printf("Ratio: %.3f\n", ratio)
 }
