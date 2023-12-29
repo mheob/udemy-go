64496f8a9910535f65ce2bab2dbb17fc62901ab6
Alex Böhm <tools@boehm.work>
2023-12-29 11:50:15 +0100

    feat: add type alias and loops to lists

--- examples/lists/make.go
+++ examples/lists/make.go
@@ -2,6 +2,12 @@ package lists
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func RunMake() {
 	runSlices()
 	fmt.Println("--------------------------------")
@@ -12,20 +18,33 @@ func runSlices() {
 	userNames := make([]string, 2, 5)
 
 	userNames[0] = "Julie"
-	userNames[1] = "Joseph"
 
 	userNames = append(userNames, "Max")
 	userNames = append(userNames, "Manuel")
 
 	fmt.Println(userNames)
+
+	fmt.Println("--------------------------------")
+
+	for index, value := range userNames {
+		fmt.Println("Index:", index)
+		fmt.Println("Value:", value)
+	}
 }
 
 func runMaps() {
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.7
 
-	fmt.Println(courseRatings)
+	courseRatings.output()
+
+	fmt.Println("--------------------------------")
+
+	for key, value := range courseRatings {
+		fmt.Println("Key:", key)
+		fmt.Println("Value:", value)
+	}
 }
